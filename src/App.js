31402c0b7c913abe88e9a8cde490e2cd85e30990
Shark Shaaark <pikamee@Sharks-MacBook-Pro.local>
2021-07-13 23:37:26 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+import IngredientIndex from './ingredient-index';
+import RecipeIndex from './recipe-index';
+import RecipePage from './recipe-page';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/ingredients">Ingredient index</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/recipe/:id" render={(props) => (
+            <RecipePage recipe_id={props.match.params.id}/>
+          )} />
+          <Route path="/ingredients">
+            <IngredientIndex />
+          </Route>
+          <Route path="/">
+            <RecipeIndex />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
